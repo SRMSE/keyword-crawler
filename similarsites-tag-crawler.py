b2b82d61a42ee5c2ba8b21cde7d89c1b143922cf
Varun Dey <varun.silversurfer@gmail.com>
2016-02-08 14:52:55 +0530

    fixed bugs in similarsites crawler. now shows log :clipboard:

--- similarsites-tag-crawler.py
+++ similarsites-tag-crawler.py
@@ -4,6 +4,19 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.test
 similarsites = db.similarsites
+bcolors={
+	"HEADER" : '\033[95m',
+    "INFO" : '\033[94m',
+    "SUCCESS" : '\033[92m',
+    "WARNING" : '\033[93m',
+    "FAIL" : '\033[91m',
+    "ENDC" : '\033[0m',
+    "BOLD" : '\033[1m',
+    "UNDERLINE" : '\033[4m'
+}
+from time import gmtime, strftime
+def put(msg,type):
+	print bcolors[type.upper()] + ""+"["+strftime("%Y-%m-%d %H:%M:%S", gmtime())+"]\t["+type.strip().capitalize()+"]\t"+str(msg)+"" + bcolors["ENDC"]
 
 url = "http://www.similarsites.com/browse"
 soup = bs(requests.get(url).content,"lxml")
@@ -15,29 +28,35 @@ for alphabet_link in list:
 
 flag = 0
 for current_alphabet in alphabet:
-	print current_alphabet
 	page = 1
 	while True:
 		try:
 			current_alphabet_soup = bs(requests.get(current_alphabet+"/"+str(page)).content,"lxml")
-			page += 1
+			
 			domains = current_alphabet_soup.find("tbody").findAll("a")
+			
 			for current_domain in domains:
 				link = "http://www.similarsites.com" +current_domain.get("href")
-				print link
 				link_soup = bs(requests.get(link).content,"lxml")
-				similar_topics = link_soup.find("section",{"id":"similarTopics"}).findAll("div",{"class":"row"})
-				i=[]
-				dic={}
-				for topics in similar_topics:
-					i.append(topics.text.split("'")[1].strip())
-				dic["tags"]=i
-				print dic
-				dic.update({"link":link})
-				similarsites.insert(dic)
-
-			print page
-
+				try:
+					similar_topics = link_soup.find("section",{"id":"similarTopics"}).findAll("div",{"class":"row"})
+					i=[]
+					dic={}
+					for topics in similar_topics:
+						i.append(topics.text.split("'")[1].strip().encode('utf-8'))
+					dic["tags"]=i
+					
+					dic.update({"link":link})
+					#similarsites.insert(dic)
+					put(dic,"SUCCESS")
+					print current_alphabet
+					print page
+					print "-------------------------------------------------------------------"
+				
+				except Exception as e:
+					put(e,"FAIL")
+					continue
+			page += 1
 		except Exception as e:
 			flag += 1
 			print e
